e219190b6c0767765f97ce8b136f61ead29afb09
uchmielewska <u.chmielewska@millionmonkeyssoftware.com>
2023-01-10 00:05:08 +0100

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:eisenhover_planner/ui/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'services/theme_services.dart';
 import 'ui/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await GetStorage.init();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 
-const Color bluishClr = Color(0xFF4e5ae8);
-
-const primaryClr = bluishClr;
-const Color darkGreyClr = Color(0xFF121212);
-Color darkHeaderClr = Color(0xFF424242);
-
 class Themes {
   static final light = ThemeData(
     primarySwatch: Colors.blue,
